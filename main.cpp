d44afc92cc9c08f8f69940ad03e91ca01afe1410
A Student <student@nowhere.edu>
2025-11-09 05:29:06 +0000

    Nov 8, 2025, 9:29 PM

--- main.cpp
+++ main.cpp
@@ -12,7 +12,84 @@ using namespace std;
 
 
 // Paste your function implementations here
+// Helper: extract value of an attribute name="VALUE"
+string getAttributeValue(const string& tag, const string& name) {
+    string key = name + "=\"";
+  size_t pos = tag.find(key);
+  if (pos == string::npos) return "";
+  pos += key.length();
+    size_t endPos = tag.find("\"", pos);
+  return tag.substr(pos, endPos - pos);}
 
+// Helper: extract the tag name (e.g., img from <img src="...">)
+string getTagName(const string& tag) {
+  size_t start = tag.find("<") + 1;
+size_t end = tag.find(" ");
+if (end == string::npos)
+    end = tag.find(">");
+return tag.substr(start, end - start);
+}
+
+// Helper: check if a tag is self-closing (like <br/> or <img/>)
+bool isSelfClosing(const string& tag) {
+    return tag[tag.size() - 2] == '/';
+}
+
+// Extract the value inside source="..."
+// Extract: returns "cat_pounce.gif"
+string getImageSource(string tag) {
+  string value = getAttributeValue(tag, "source");
+  return value;
+}
+
+// Replace underscores with spaces
+string underscoresToSpaces(string str) {
+    for (size_t i = 0; i < str.size(); i++) {
+      if (str[i] == '_')
+      str[i] = ' ';
+    }
+    return str;
+  }
+
+// Extract width="100px" -> returns 100
+int getImageWidth(string tag) {
+  string value = getAttributeValue(tag, "width");
+  int num = stoi(value.substr(0, value.size() - 2));
+return num;
+}
+
+// Extract height="200px" -> rerturns 200
+int getImageHeight(string tag) {
+  string value = getAttributeValue(tag, "height");
+  int num = stoi(value.substr(0, value. size () - 2));
+return num;
+}
+
+// Get vertical padding (first number in padding="10px 5px")
+int getVerticalPadding(string tag) {
+  string value = getAttributeValue(tag, "padding");
+  if (value == "") return 0; // no padding attribute
+  
+  size_t space = value.find(" ");
+string first = (space == string::npos) ? value : value.substr(0, space);
+return stoi(first.substr(0, first.size() - 2));
+}
+
+// Get horizontal padding (second number in padding="10px 5px")
+int getHorizontalPadding(string tag) {
+    string value = getAttributeValue(tag, "padding");
+    if (value == "") return 0;  // no padding attribute
+
+    size_t space = value.find(' ');
+    if (space == string::npos) {
+        // only one value -> applies to both directions
+        return stoi(value.substr(0, value.size() - 2));
+    } else {
+      // take second token, remove "px"
+        string second = value.substr(space + 1);
+        return stoi(second.substr(0, second.size() - 2));
+    }
+}
 
 int main()
 {
